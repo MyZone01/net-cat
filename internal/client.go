0c21812a3d691524051a9c34fcbfc732521b1482
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-07-14 11:33:19 +0000

    Sync.Map usage to broadcast etablished

--- internal/client.go
+++ internal/client.go
@@ -1,41 +1,89 @@
-// package internal
+package internal
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"net"
-// 	"os"
-// )
+//
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+	"sync"
+)
 
-// func Client(connect *net.Conn) (string, error) {
+// checking if a name is provide and return a true that will allow access to the chat
+func accessChat(connect net.Conn, connectAddr net.Addr, group *sync.Map) (bool, string) {
+	name := ""
+	count := 0
 
-// 	// connect, err := net.Dial("tcp", os.Args[1])
-// 	// if err != nil {
-// 	// fmt.Println(err)
-// 	// fmt.Println("Choose between 1024 & 65535")
-// 	// }
+	for name == "" || len(name) > 20 {
+		if count == 3 {
+			connect.Write([]byte("Please provide a non-empty name"))
+		} else if count > 10 {
+			connect.Write([]byte("You're a funny One !"))
+		}
+		connect.Write([]byte("[ENTER YOUR NAME]: "))
+		count++
+		naming, err := bufio.NewReader(connect).ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				// leaveMessage(connect)
+				Addr := connect.RemoteAddr().String()
+				fmt.Println("Joining Cancelled ... from : " + Addr)
+				return false, naming
+			} else {
+				fmt.Println(err)
+				return false, naming
+			}
+		}
+		name = strings.TrimSuffix(naming, "\n")
+		welcomeMessage(connect, group, name)
+		// name = "[" + name + "]"
+		group.Store(name, connect)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Joining room cancel !!!")
+				// return
+			} else {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}
+	return true, name
+}
 
-// goodName:
-// 	nameRequest := bufio.NewReader(os.Stdin)
-// 	(*connect).Write([]byte("Welcome to ROOM"))
-// 	(*connect).Write([]byte("[ENTER YOUR NAME]:"))
-// 	name, err := nameRequest.ReadString('\n')
-// 	if err != nil {
-// 		fmt.Println("Aïeee")
-// 	}
-// 	if len(name) == 0 {
-// 		goto goodName
-// 	}
+func Clients(connect *net.Conn) (string, error) {
 
-// 	for {
-// 		greeting()
-// 		timing := timer()
-// 		reader := bufio.NewReader(os.Stdin)
-// 		text, _ := reader.ReadString('\n')
-// 		fmt.Fprint((*connect), text+"\n")
+	// 	// connect, err := net.Dial("tcp", os.Args[1])
+	// 	// if err != nil {
+	// 	// fmt.Println(err)
+	// 	// fmt.Println("Choose between 1024 & 65535")
+	// 	// }
 
-// 		message, _ := bufio.NewReader(*connect).ReadString('\n')
-// 		fmt.Print(timing, "[", name, "]", message)
-// 	}
-// 	// }
-// }
+	// goodName:
+	nameRequest := bufio.NewReader(os.Stdin)
+	// 	(*connect).Write([]byte("Welcome to ROOM"))
+	// 	(*connect).Write([]byte("[ENTER YOUR NAME]:"))
+	name, err := nameRequest.ReadString('\n')
+	// 	if err != nil {
+	// 		fmt.Println("Aïeee")
+	// 	}
+	// 	if len(name) == 0 {
+	// 		goto goodName
+	// 	}
+
+	// 	for {
+	// 		greeting()
+	// 		timing := timer()
+	// 		reader := bufio.NewReader(os.Stdin)
+	// 		text, _ := reader.ReadString('\n')
+	// 		fmt.Fprint((*connect), text+"\n")
+
+	//		message, _ := bufio.NewReader(*connect).ReadString('\n')
+	//		fmt.Print(timing, "[", name, "]", message)
+	//	}
+	//
+	return name, err
+	// // }
+}
--- internal/server.go
+++ internal/server.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -22,6 +23,7 @@ type Client struct {
 }
 
 type message struct {
+	name    string
 	text    string
 	address string
 }
@@ -41,29 +43,39 @@ func Server() {
 		return
 	}
 	defer listener.Close()
+	fmt.Println("Server Ready on: ", PORT)
 
-	for len(clients) < 2 {
+	var connectMap = &sync.Map{}
+	// var connMap map[string]net.Conn
 
+	for {
 		connection, err := listener.Accept()
+		connectionAddr := listener.Addr()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		} //else {
 		connection.Write(greeting())
 		// name, _ := Client(&connection)
-		access, name := accessChat(connection)
+		access, name := accessChat(connection, connectionAddr, connectMap)
+		fmt.Println(name + " joined")
 		if access {
-			go connectionHandler(connection, name)
+			connectMap.Store(name, connection)
+			// connMap[name] = connection
+			// fmt.Println(connectMap)
+			go connectionHandler(connection, name, connectMap)
 		}
 	}
 	// }
 
 }
 
-func connectionHandler(connection net.Conn, name string) {
-	// connection.Write([]byte("[ENTER YOUR NAME]: "))
+func connectionHandler(connection net.Conn, name string, group *sync.Map) {
+
+	// defer closeConnection(connection, group)
 
 	for {
+
 		timing := timer()
 		infos := timing + name + ":" // + string(chatData)
 		// fmt.Print(infos)
@@ -72,63 +84,52 @@ func connectionHandler(connection net.Conn, name string) {
 		chatData, err := bufio.NewReader(connection).ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
-				fmt.Println("Has LEFT !!!")
+				// fmt.Println(name + "Has LEFT !!!")
+				closeConnection(connection, group, name)
+				group.Delete(name)
 				return
 			} else {
 				fmt.Println(err)
 				return
 			}
 		}
-		if strings.TrimSpace(string(chatData)) == "STOP" {
-			fmt.Print("Exiting Chat ... !")
-			return
+
+		// message := chatData
+
+		group.Range(func(key, value any) bool {
+			// messageBrodcast(connection, value.(net.Conn), message)
+			if value.(net.Conn) != connection {
+				// value.(net.Conn).Write([]byte(message))
+				// timing := timer()
+				// infos := timing + fmt.Sprintf("%s", key) + ":"
+				infos := "\r\033[K" + chatData + label(fmt.Sprintf("%s", key)) //timing + name + ":"
+				value.(net.Conn).Write([]byte(infos))
+			}
+			return true
+		})
+
+		if strings.TrimSpace(string(chatData)) == "STOP" || strings.TrimSpace(string(chatData)) == "EXIT" {
+			fmt.Print(name + "Exiting Chat ... !")
+			connection.Write([]byte("You've successfully logout"))
+			group.Delete(name)
+			connection.Close()
 		}
 
 	}
 
 }
 
+func messageBroadcast(group *sync.Map, connect net.Conn) {
+
+}
+
 func newMessage(msg string, connection net.Conn) message {
+
 	address := connection.RemoteAddr().String()
 	return message{
 		text:    address + msg,
 		address: address,
 	}
-}
-
-func accessChat(connect net.Conn) (bool, string) {
-	naming := ""
-	for naming == "" {
-		connect.Write([]byte("[ENTER YOUR NAME]: "))
-		nami, err := bufio.NewReader(connect).ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				leaveMessage(connect)
-				//fmt.Println("Has LEFT !!!")
-				return false, naming
-			} else {
-				fmt.Println(err)
-				return false, naming
-			}
-		}
-		nami = strings.TrimSuffix(nami, "\n")
-		naming = nami
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Joining room cancel !!!")
-				// return
-			} else {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-		// goodPseudo:
-		// connect.Write([]byte(greeting()))
-		// connect.Write([]byte("[ENTER YOUR NAME]:"))
-		// goto goodPseudo
-	}
-	name := "[" + naming + "]"
-	return true, name
 }
 
 func greeting() []byte {
@@ -139,6 +140,13 @@ func greeting() []byte {
 	return file
 }
 
+func label(name string) string {
+	timing := timer()
+	infos := timing + "[" + name + "]:" // + string(chatData)
+	// fmt.Print(infos)
+	return infos
+}
+
 func timer() string {
 	t := time.Now()
 	formatted := fmt.Sprintf("[%d-%02d-%02d %02d:%02d:%02d]",
@@ -147,24 +155,54 @@ func timer() string {
 	return formatted
 }
 
-func messageBrodcast() {
-	for {
-		select {
-		case msg := <-messages:
-			for _, connect := range clients {
-				if msg.address == connect.RemoteAddr().String() {
-					continue
-				}
-				fmt.Fprintln(connect, msg.text)
-			}
-		case msg := <-leaving:
-			for _, connect := range clients {
-				fmt.Fprintln(connect, msg.text)
-			}
+func closeConnection(conn net.Conn, group *sync.Map, name string) {
+	conn.Close()
+	group.Delete(name)
+	group.Range(func(key, value any) bool {
+		if key != name {
+			value.(net.Conn).Write([]byte(name + " has left our chat ... "))
 		}
-	}
+		return true
+	})
 }
 
-func leaveMessage(connect net.Conn) {
-
+func welcomeMessage(conn net.Conn, group *sync.Map, name string) {
+	// conn.Close()
+	// group.Delete(name)
+	group.Range(func(key, value any) bool {
+		if key == name {
+		} else {
+			value.(net.Conn).Write([]byte("\r\033[K" + name + " has joined our chat ... "))
+		}
+		return true
+	})
 }
+
+// fmt.Println(*group)
+// if key == name {
+// 	return true
+// } else {
+// 	Write([]byte(message))
+// return true
+
+// })
+// for {
+// 	select {
+// 	case msg := <-messages:
+// 		for _, connect := range clients {
+// 			if msg.address == connect.RemoteAddr().String() {
+// 				continue
+// 			}
+// 			fmt.Fprintln(connect, msg.text)
+// 		}
+// 	case msg := <-leaving:
+// 		for _, connect := range clients {
+// 			fmt.Fprintln(connect, msg.text)
+// 		}
+// 	}
+// }
+// }
+
+/*func leaveMessage(connect net.Conn, key, value interface{}, name string) {
+
+}*/
